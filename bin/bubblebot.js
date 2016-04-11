9caaa43a7ae8595e08e18b9f8ad0179df8a7d568
Josh Haas <jphaas@gmail.com>
2016-04-11 16:13:29 -0400

    making the command line script more generic

--- bin/bubblebot.js
+++ bin/bubblebot.js
@@ -5,23 +5,25 @@ child_process = require('child_process');
 
 
 run_command = function() {
-    if (command === 'install') {
-        bubblebot.install();
-    } else if (command === 'publish') {
-        bubblebot.publish();
-    } else {
+    if (!bubblebot[command]) {
         bubblebot.print_help();
         process.exit()
+    } else {
+        bubblebot[command].apply(this, process.argv.slice(3));
     }
 }
 
 
-
+var not_installed;
 try {
     bubblebot = require(process.cwd() + '/node_modules/bubblebot');
-    run_command();
+    not_installed = false;
 } catch (err) {
+    not_installed = true;
+}
+if (not_installed) {
     if (command === 'install') {
+        console.log('Adding bubblebot to package.json and installing: npm install --save bublebot');
         child_process.exec('npm install --save bubblebot', {cwd: process.cwd()}, function(err, stdout, stderr) {
             if (err) {
                 console.log(stdout);
@@ -36,6 +38,8 @@ try {
         console.log('Bubblebot is not installed in this directory.  To install, run "bubblebot install"');
         process.exit();
     }
+} else {
+    run_command();
 }
 
 
